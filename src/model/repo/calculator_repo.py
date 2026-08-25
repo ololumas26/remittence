5b22fd994a526461633ef22d4ecb6c899b1e39e9
Eliseu Franco Samulolo <97409528+ololumas26@users.noreply.github.com>
2026-08-25 15:05:22 +0100

    Calculator interface removed

--- src/model/repo/calculator_repo.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from decimal import Decimal
-
-
-class CalculatorRepository(ABC):
-
-    @abstractmethod
-    def calculate(self, amount : Decimal, exchange_rate : Decimal):
-        raise NotImplementedError
